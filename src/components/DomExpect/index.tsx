8f46c662a50a8ea4f483cf19aef6d3a05e7131ef
李云富 <liyunfu@liyunfus-MacBook-Pro.local>
2023-02-17 15:25:15 +0800

    基于组件库的验证

--- src/components/DomExpect/index.tsx
+++ src/components/DomExpect/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { Form } from "@douyinfe/semi-ui";
 
 interface IProps {}
 
@@ -20,18 +21,42 @@ export const DomExpect: FC<IProps> = ({}) => {
           disabled
           aria-disabled
           defaultValue="alien"
+          aria-label="form_username"
         />
-        <input type="number" name="age" defaultValue={23} required />
+        <input
+          type="number"
+          name="age"
+          defaultValue={23}
+          required
+          aria-label="form_age"
+        />
         <input
           type="radio"
           name="sex"
           value="man"
           defaultChecked
           aria-checked
+          aria-label="form_sex"
         />
-        <input type="radio" name="sex" value="woman" />
+        <input type="radio" name="sex" value="woman" aria-label="form_sex" />
       </form>
 
+      {/* semi表单验证 */}
+      <Form
+        initValues={{ username: "alien", age: 23, sex: "man", hobby: "code" }}
+        aria-label="semi-form"
+      >
+        <Form.Input field="username" disabled name="username" />
+        <Form.InputNumber field="age" required name="age" />
+        <Form.RadioGroup field="sex" name="sex">
+          <Form.Radio value="man" />
+          <Form.Radio value="woman" />
+        </Form.RadioGroup>
+        <Form.Select field="hobby" name="hobby">
+          <Form.Select.Option value="code">code</Form.Select.Option>
+          <Form.Select.Option value="read">read</Form.Select.Option>
+        </Form.Select>
+      </Form>
       {/* 代码层面验证 */}
       <div
         role="note"
