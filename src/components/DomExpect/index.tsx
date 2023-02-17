4d8462d3583a87cad3cfdc2ed961bc96b1a855e9
李云富 <liyunfu@liyunfus-MacBook-Pro.local>
2023-02-17 14:42:46 +0800

    🌟feat: 表单验证断言示例

--- src/components/DomExpect/index.tsx
+++ src/components/DomExpect/index.tsx
@@ -10,6 +10,25 @@ export const DomExpect: FC<IProps> = ({}) => {
         1234
       </div>
       <div role="note">1234</div>
+
+      <form aria-label="form">
+        <input
+          type="text"
+          name="username"
+          disabled
+          aria-disabled
+          defaultValue="alien"
+        />
+        <input type="number" name="age" defaultValue={23} required />
+        <input
+          type="radio"
+          name="sex"
+          value="man"
+          defaultChecked
+          aria-checked
+        />
+        <input type="radio" name="sex" value="woman" />
+      </form>
     </div>
   );
 };
